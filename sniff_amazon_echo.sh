8fd13fe7effbbcc465fc14fa2b77d9cb63793989
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2019-12-31 22:54:42 -0500

    pcap for the amazon echo alexa devices only

---
+++ sniff_amazon_echo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+tshark -i eno1 -w ~/Documents/PCAPs/amazon_echo_devices.`date +%Y%m%d.%H.%M`.pcap -f "not dst net 169.254.0.0/16 and not multicast and not broadcast and not arp and ( host 192.168.1.130 or host 192.168.1.170 or host 192.168.1.231)"  -a duration:$1
+
