805b43a50176177ae317d68d3136f2b285387373
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2020-01-12 17:11:53 -0500

    Updated scripts for dynamic port, now that I have a sniffer interface

--- sniff_dns.sh
+++ sniff_dns.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tshark -i eno1 -w dns_requests.`date +%Y%m%d.%H.%M`.pcap -f "port 53" -a duration:$1
+tshark -i $1 -w ~/Documents/PCAPs/dns_requests.`date +%Y%m%d.%H.%M`.pcap -f "port 53" -a duration:$2
