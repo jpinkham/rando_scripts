f866fe204845b828cb9b35cb4769d0701a3a0395
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2020-01-26 14:03:44 -0500

    Play ocean sounds on Squeezebox

---
+++ squeezebox_oceansounds.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl --silent --show-error  'http://192.168.11.14:9000/anyurl?p0=playlist&p1=play&p2=loop%3A%2F%2Fcontent.mysqueezebox.com%2Fstatic%2Fsounds%2Fnatural%2Focean_surf.mp3&p3=Ocean%20Surf&player=00%3A04%3A20%3A06%3Ac2%3Aec&_dc=1580064950761' -H 'User-Agent: STRONGBAD/8675309' --compressed 
+
