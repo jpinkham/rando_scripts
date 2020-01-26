db76e4215231e84b190e25580d87f9cecc7f0925
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2020-01-26 13:50:14 -0500

    Using JSONRPC in SlimServer to power on Squeezebox

---
+++ squeezebox_power.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl --silent --show-error  'http://192.168.11.14:9000/jsonrpc.js' -H 'User-Agent: STRONGBAD/8675309' --compressed -H 'Cookie: Squeezebox-player=00%3A04%3A20%3A06%3Ac2%3Aec;' --data '{"id":1,"method":"slim.request","params":["00:04:20:06:c2:ec",["power","1"]]}' | jq
