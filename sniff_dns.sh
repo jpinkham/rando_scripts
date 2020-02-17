797260a8bfd33b9aee560511ad52d3fd12ecac60
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2020-02-16 22:48:17 -0500

    removing sniffer files. borked the move

--- sniff_dns.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tshark -i $1 -w ~/Documents/PCAPs/dns_requests.`date +%Y%m%d.%H.%M`.pcap -f "port 53" -a duration:$2
--- sniff_iot_devices.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-tshark -i $1 -w ~/Documents/PCAPs/IoTDevices-nonintranettraffic.`date +%Y%m%d.%H.%M`.pcap -f "not dst net 169.254.0.0/16 and not multicast and not broadcast and not arp and ( host 192.168.1.49 or host 192.168.1.92 or host 192.168.1.109 or host 192.168.1.130 or host 192.168.1.169 or host 192.168.1.170 or host 192.168.1.180 or host 192.168.1.231 or host 192.168.1.241) and not (src net 192.168.1.0/24 and dst net 192.168.1.0/24)"  -a duration:$2
-
