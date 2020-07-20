9c50e2cfabb254006629ed79c9b8f5b6927f9886
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2020-07-20 11:17:30 +0000

    Added syslog and messages file so it works on all my hosts

--- tail_syslog.sh
+++ tail_syslog.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo tail -n 2500 --follow=name --retry /var/log/messages|grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo'
+sudo tail -n 2500 --follow=name --retry /var/log/messages /var/log/syslog |grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo'
