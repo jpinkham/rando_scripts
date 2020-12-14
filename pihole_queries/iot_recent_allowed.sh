91d093d5b4948017db431f967b19810435fc033a
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2020-12-13 21:06:16 -0500

    Updated IP for phone to current one

--- pihole_queries/iot_recent_allowed.sh
+++ pihole_queries/iot_recent_allowed.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sqlite3 /etc/pihole/pihole-FTL.db "SELECT domain FROM queries WHERE client IN('192.168.11.118','192.168.11.3','192.168.11.139','192.168.11.51') AND status=2 ORDER BY id DESC LIMIT $1" |sort|uniq -c|sort -g -r|head
+sqlite3 /etc/pihole/pihole-FTL.db "SELECT domain FROM queries WHERE client IN('192.168.2.18','192.168.11.3','192.168.11.139','192.168.11.51') AND status=2 ORDER BY id DESC LIMIT 1" |sort|uniq -c|sort -g -r|head
--- pihole_queries/iphone_recent_blocked.sh
+++ pihole_queries/iphone_recent_blocked.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sqlite3 /etc/pihole/pihole-FTL.db "SELECT domain FROM queries WHERE client = '192.168.11.118' AND status=1 ORDER BY id DESC LIMIT $1;"|sort|uniq -c|sort -g -r|head
+sqlite3 /etc/pihole/pihole-FTL.db "SELECT domain FROM queries WHERE client = '192.168.2.18' AND status=1 ORDER BY id DESC LIMIT 1;"|sort|uniq -c|sort -g -r|head
