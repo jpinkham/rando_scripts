5af3cc03e476d081c7ed23c16e2b8a5ca723343e
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2021-04-18 19:04:04 -0400

    whatismyip.js now works as a Scriptable-powered widget

--- Scriptable/whatismyip.js
+++ Scriptable/whatismyip.js
@@ -1,22 +1,39 @@
 //iOS Scriptable script
 // Displays current public IP
 //
-const ip_url = "https://whatismyip.host/ipv4"
+ip_url = "https://whatismyip.host/ipv4";
 
-try {
-    writeLOG('Fetching url: ${ip_url}');
-    const request = new Request(ip_url);
-    response = request.loadJSON();
- } catch (error) {
-      writeLOG('Couldn\'t fetch ${ip_url}');
- }
- try {
-    if (response.code != 200) {
-      writeLOG('Something went wrong. Received non-200 response code');
-      return;
-    }
-  } catch(error) {
-    writeLOG("JSON Data: " + JSON.stringify(response));
-  }
 
-return response.ipv4
+new log("Fetching url: " + ip_url);
+   
+let req = new Request(ip_url)
+let json = await req.loadJSON()
+
+// let alert = new Alert();
+// alert.title = 'My IP';
+// alert.message = `ip = ${json.ipv4}`;
+// alert.addCancelAction("Okay");
+// alert.presentAlert();
+
+let widget = createWidget("IP", json.ipv4)
+if (config.runsInWidget) {
+  // create and show widget
+  Script.setWidget(widget)
+  Script.complete()
+}
+else {
+  widget.presentSmall()
+}
+
+function createWidget(title, ipaddress, widgeturl) {
+  console.log("Title: "+title)
+  let w = new ListWidget()
+  w.backgroundColor = new Color("#1A1A1A")
+  w.addText("External IP:").centerAlignText()
+  let titleTxt = w.addText(ipaddress)
+  titleTxt.textColor = Color.white()
+  titleTxt.font = Font.mediumMonospacedSystemFont(16)
+  titleTxt.centerAlignText()
+  w.setPadding(0, 5, 0, 0)
+  return w
+}
