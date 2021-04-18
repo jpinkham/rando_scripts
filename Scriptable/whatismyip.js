3b9e3aeadb72a7a52173f94ebbc1ea4c8bef19c9
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2021-04-18 19:08:32 -0400

    Removed unused code from working version I built in Scriptable.

--- Scriptable/whatismyip.js
+++ Scriptable/whatismyip.js
@@ -3,37 +3,26 @@
 //
 ip_url = "https://whatismyip.host/ipv4";
 
-
-new log("Fetching url: " + ip_url);
-   
 let req = new Request(ip_url)
 let json = await req.loadJSON()
-
-// let alert = new Alert();
-// alert.title = 'My IP';
-// alert.message = `ip = ${json.ipv4}`;
-// alert.addCancelAction("Okay");
-// alert.presentAlert();
-
 let widget = createWidget("IP", json.ipv4)
 if (config.runsInWidget) {
-  // create and show widget
-  Script.setWidget(widget)
-  Script.complete()
+    // create and show widget
+    Script.setWidget(widget)
+    Script.complete()
 }
 else {
-  widget.presentSmall()
+    widget.presentSmall()
 }
 
-function createWidget(title, ipaddress, widgeturl) {
-  console.log("Title: "+title)
-  let w = new ListWidget()
-  w.backgroundColor = new Color("#1A1A1A")
-  w.addText("External IP:").centerAlignText()
-  let titleTxt = w.addText(ipaddress)
-  titleTxt.textColor = Color.white()
-  titleTxt.font = Font.mediumMonospacedSystemFont(16)
-  titleTxt.centerAlignText()
-  w.setPadding(0, 5, 0, 0)
-  return w
+function createWidget(title, ipaddress) {
+    let w = new ListWidget()
+    w.backgroundColor = new Color("#1A1A1A")
+    w.addText("External IP:").centerAlignText()
+    let titleTxt = w.addText(ipaddress)
+    titleTxt.textColor = Color.white()
+    titleTxt.font = Font.mediumMonospacedSystemFont(16)
+    titleTxt.centerAlignText()
+    w.setPadding(0, 5, 0, 0)
+    return w
 }
