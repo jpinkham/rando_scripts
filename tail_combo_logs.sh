232c924d46409e3756e45ed088dcb7fc6e3b4625
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2021-09-27 07:54:44 -0400

    Updated combo syslog tail to use custom filepath on TheCheat, updated syslog tail to strip out hostname (to save space) and changed both to use bash not sh

--- tail_combo_logs.sh
+++ tail_combo_logs.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
-sudo tail -n 2500 --follow=name --retry /mnt/thecheat_storage/syslog/all_remote_syslogs.log|grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo'
+#!/usr/bin/env bash
+
+FILE="all_remote_syslogs.log"
+
+if [[ "$HOSTNAME" == "TheCheat" ]]; then
+    DIRNAME="/usr/share/extra_storage/syslog"
+else
+    DIRNAME="/mnt/thecheat_storage/syslog"
+fi
+
+sudo tail -n 2500 --follow=name --retry $DIRNAME/$FILE|grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo'
--- tail_syslog.sh
+++ tail_syslog.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
-sudo tail -n 2500 --follow=name --retry /var/log/messages /var/log/syslog |grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo'
+#!/usr/bin/env bash
+
+sudo tail -n 2500 --follow=name --retry /var/log/messages /var/log/syslog |grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo' | sed "s/$HOSTNAME //"
