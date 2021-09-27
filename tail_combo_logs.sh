ce4aed975bb2a967fc9fd6d47d01f5731046de3c
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2021-09-27 09:17:13 -0400

    filter current host from combo syslog. WIP: converting log entry ip address to hostname

--- tail_combo_logs.sh
+++ tail_combo_logs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 FILE="all_remote_syslogs.log"
+IPADDRESS=$(ifconfig|grep 192|awk '{print $2}')
 
 if [[ "$HOSTNAME" == "TheCheat" ]]; then
     DIRNAME="/usr/share/extra_storage/syslog"
@@ -8,4 +9,7 @@ else
     DIRNAME="/mnt/thecheat_storage/syslog"
 fi
 
-sudo tail -n 2500 --follow=name --retry $DIRNAME/$FILE|grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo'
+# filter log entries we don't care about, including entries from current host
+# pipe through local_dns_lookups.sh to dynamically convert ip addresses to hostnames
+sudo tail -n 2500 --follow=name --retry $DIRNAME/$FILE|grep -ivE "rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo|$IPADDRESS"
+# figure out how to extract ip from message and run it against lical_dns_lookup.sh in order to show short hostnames
