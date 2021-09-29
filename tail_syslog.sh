bb39afbfe36cbc4fde82eb13ecd12637ba0b2e30
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2021-09-29 08:39:07 -0400

    converted to use journalctl so sudo won't be needed

--- tail_syslog.sh
+++ tail_syslog.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo tail -n 2500 --follow=name --retry /var/log/messages /var/log/syslog |grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo' | sed "s/$HOSTNAME //"
+journalctl --lines 50 --follow --no-hostname --output short-precise | grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo'
