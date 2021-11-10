26578f8da60c55500d4d3a728617c255a6fe204c
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2021-11-10 11:44:43 -0500

    Slightly shortened agent expire time

--- is_agent_running.sh
+++ is_agent_running.sh
@@ -5,14 +5,14 @@ AGENT_OUTPUT=`ssh-add -l 2>&1`
 if [[ $AGENT_OUTPUT == "Error connecting to agent: No such file or directory" ]] || [[ $AGENT_OUTPUT == "Error connecting to agent: Connection refused" ]] || [[ $AGENT_OUTPUT == "Could not open a connection to your authentication agent." ]] || [[ $SSH_AGENT_PID == "" ]]; then
     echo "No SSH agent running. Starting one and adding keys"
     eval $(ssh-agent)
-    ssh-add -t 9999999 ~/.ssh/github_rsa > /dev/null 2>&1
-    ssh-add -t 9999999 ~/.ssh/id_github  > /dev/null 2>&1
-    ssh-add -t 9999999 ~/.ssh/id_rsa  > /dev/null 2>&1
+    ssh-add -t 999999 ~/.ssh/github_rsa > /dev/null 2>&1
+    ssh-add -t 999999 ~/.ssh/id_github  > /dev/null 2>&1
+    ssh-add -t 999999 ~/.ssh/id_rsa  > /dev/null 2>&1
 elif [ "$AGENT_OUTPUT" == "The agent has no identities." ]; then
     echo "SSH agent running but contains no keys. Adding."
-    ssh-add -t 9999999 ~/.ssh/github_rsa > /dev/null 2>&1
-    ssh-add -t 9999999 ~/.ssh/id_github  > /dev/null 2>&1
-    ssh-add -t 9999999 ~/.ssh/id_rsa  > /dev/null 2>&1
+    ssh-add -t 999999 ~/.ssh/github_rsa > /dev/null 2>&1
+    ssh-add -t 999999 ~/.ssh/id_github  > /dev/null 2>&1
+    ssh-add -t 999999 ~/.ssh/id_rsa  > /dev/null 2>&1
 else
     echo "SSH agent already running and already contains keys"
     ssh-add -t 9999999 ~/.ssh/id_github  > /dev/null 2>&1
@@ -33,5 +33,3 @@ echo "SSH_AGENT_PID = $SSH_AGENT_PID"
 echo "SSH_AUTH_SOCK = $SSH_AUTH_SOCK"
 echo "---------------------------------------------------"
 
-#echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
-#sleep 2
