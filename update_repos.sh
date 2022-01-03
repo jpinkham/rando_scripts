9be49fb7da43675157b9f00d86821b911e9e9a84
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2022-01-03 17:51:07 +0000

    fixed shellcheck issues

--- update_repos.sh
+++ update_repos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2035,SC2045
 ###################################################
 # Name:    update_repos.sh
 # Purpose: for git repositories that you have cloned locally but are not planning to modify
@@ -8,11 +9,17 @@ SOURCE_DIR=$1 && echo "Using source directory >$SOURCE_DIR<"
 
 cd "$SOURCE_DIR" || { echo "Could not change to directory >$SOURCE_DIR<"; exit 1; }
 
-for REPO in `ls -d */ | sed 's/\///' `
+for REPO in $(ls -d *) 
+ # SC2045: Iterating over ls output is fragile. Use globs.
+ # SC2035: Use ./*glob* or -- *glob* so names with dashes won't become options
 	do
 		echo "Updating $REPO"
 		cd "$REPO" || { echo "Could not change to directory >$REPO<"; exit 1; }
+
+        # Adds 4 spaces in front of output from git command, without including default newline
+        echo -n "    "  
 		git pull
+
 		cd ..
 done
 
