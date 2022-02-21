b661962f2cd0bc3a80b847cc6e8e6af9938669a3
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2022-02-21 23:36:49 +0000

    tail_syslog.sh: added a few more keywords to ignore in output

--- tail_syslog.sh
+++ tail_syslog.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-journalctl --lines 50 --follow --no-hostname --output short-precise | grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo'
+journalctl --lines 500 --follow --no-hostname --output short-precise | grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo|Deactivated|X11'
