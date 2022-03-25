58411fb7e3d58f572b4e0a05bc6114546da78881
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2022-03-25 10:46:09 -0400

    is_agent_running: created function for adding SSH keys, which loads all the keys it can find with 'id_' name

--- is_agent_running.sh
+++ is_agent_running.sh
@@ -2,20 +2,22 @@
 
 AGENT_OUTPUT=`ssh-add -l 2>&1`
 
+function add_keys() {
+    # Load every private key that starts with "id_"
+    for KEY in $(find ~/.ssh/ -name "id_*"|grep -v pub); do
+        ssh-add -t 999999 $KEY > /dev/null 2>&1;
+    done
+}
+
 if [[ $AGENT_OUTPUT == "Error connecting to agent: No such file or directory" ]] || [[ $AGENT_OUTPUT == "Error connecting to agent: Connection refused" ]] || [[ $AGENT_OUTPUT == "Could not open a connection to your authentication agent." ]] || [[ $SSH_AGENT_PID == "" ]]; then
     echo "No SSH agent running. Starting one and adding keys"
     eval $(ssh-agent)
-    ssh-add -t 999999 ~/.ssh/github_rsa > /dev/null 2>&1
-    ssh-add -t 999999 ~/.ssh/id_github  > /dev/null 2>&1
-    ssh-add -t 999999 ~/.ssh/id_rsa  > /dev/null 2>&1
+    add_keys
 elif [ "$AGENT_OUTPUT" == "The agent has no identities." ]; then
     echo "SSH agent running but contains no keys. Adding."
-    ssh-add -t 999999 ~/.ssh/github_rsa > /dev/null 2>&1
-    ssh-add -t 999999 ~/.ssh/id_github  > /dev/null 2>&1
-    ssh-add -t 999999 ~/.ssh/id_rsa  > /dev/null 2>&1
+    add_keys
 else
-    echo "SSH agent already running and already contains keys"
-    ssh-add -t 9999999 ~/.ssh/id_github  > /dev/null 2>&1
+    echo "SSH agent already running and contains keys"
 fi
 
 export SSH_AGENT_PID
