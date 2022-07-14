2232421107dd0195031c911d5c296e3013ddaa3c
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2022-07-13 21:20:02 -0400

    Added strings to filter out of log messages

--- tail_syslog.sh
+++ tail_syslog.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-journalctl --lines 500 --follow --no-hostname --output short-precise | grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo|Deactivated|X11'
+journalctl --lines 500 --follow --no-hostname --output short-precise | grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo|Deactivated|X11|Assignment outside'
