9ca599647d7013dd9e7598ead128f90a8e3b9139
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2022-09-02 19:40:28 -0400

    tail_syslog: added more things to ignore

--- tail_syslog.sh
+++ tail_syslog.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-journalctl --lines 500 --follow --no-hostname --output short-precise | grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo|Deactivated|X11|Assignment outside'
+journalctl --lines 1000 --follow --no-hostname --output short-precise | grep -ivE 'rngd|cron|php|locale|accordingly|reloading.|apt-daily|daily apt|cleanup|calibre|slice|session|succeeded|tracker|sudo|Deactivated|X11|Assignment outside|gtk_widget_get_scale_factor|whoopsie|Advantage'
