fdb2ffc5d56dfbd2b789bf3a7040ccc9f4276b39
Jennifer Pinkham <jennifer.pinkham@owasp.org>
2022-10-01 10:11:42 -0400

    removed squeezebox cookie value as it is no longer needed

--- squeezebox_command.sh
+++ squeezebox_command.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # control Squeezebox
-SBSESSION=$(cat $HOME/.config/squeezebox.conf)
 # player = 00:04:20:07:D3:27
 curl 'http://192.168.2.11:9000/jsonrpc.js' \
 --silent \
 -X POST \
 -H 'Content-Type: application/json' \
 -H 'Host: mysqueezebox.com' \
---cookie "sdi_squeezenetwork_session=$SBSESSION; Squeezebox-player=00%3A04%3A20%3A07%3Ad3%3A27" \
+--cookie "Squeezebox-player=00%3A04%3A20%3A07%3Ad3%3A27" \
 -d "{\"id\":1,\"method\":\"slim.request\",\"params\":[\"00:04:20:07:d3:27\",[\"$1\"]]}" | jq .
 
